0e029ab10ed3f57f2809a9ab76daf186599fc691
Clement <clementmaganza@gmail.com>
2024-09-28 19:20:36 +0200

    ajout du formulaire sans css

--- script/script.js
+++ script/script.js
@@ -11,24 +11,34 @@ boutonDemarage.addEventListener('click',()=>{
     verifDemaree++
 })
 
+let departMinutes
 
-const departMinutes = 0.05
-const departPause = 0.1
+if(window.localStorage.getItem("tempsCronoTravail") == null){
+    departMinutes = 25;
+}else {
+    departMinutes = window.localStorage.getItem("tempsCronoTravail")
+}
+
+let departPause
+if(window.localStorage.getItem("tempsCronoPause") == null){
+    departPause = 5
+}else {
+    departPause = window.localStorage.getItem("tempsCronoPause")
+}
+
+
 let pause = false
 const tempsElement = document.getElementById("temps")
 
+
 let temps = departMinutes * 60
 
-
-horloge();
+horloge()
 
 
 
 function Demarer(){
     boucle = setInterval(() => {
-        
-        
-        
         if(temps == 0){
             if(pause == false){
                 temps = departPause * 60  + 1
@@ -76,6 +86,7 @@ function verif(){
 }
 
 function horloge(){
+    
     let minutes = parseInt(temps / 60, 10)
     let secondes = parseInt(temps % 60, 10)
     
@@ -89,3 +100,32 @@ function horloge(){
 
 let boutonValidee = document.getElementById("validee")
 let boutonAnnulee = document.getElementById("annulee")
+let tempsTravail = document.getElementById("cronoTravail")
+let tempsPause = document.getElementById("cronoPause")
+
+
+boutonValidee.addEventListener('click',()=>{
+    change()
+})
+boutonParametrer.addEventListener('click',()=>{
+    clearInterval(boucle)
+    temps = departMinutes * 60
+    horloge();
+    document.querySelector("#demaree").innerHTML = '<i class="fa-solid fa-play fa-3x"></i>';
+    corp.style.backgroundColor = 'red'
+    demaree.style.backgroundColor = 'red'
+    parametrer.style.backgroundColor= 'red'
+    pauseText.style.color = 'gray'
+    travailText.style.color = 'white'
+    verifDemaree = 0
+})
+
+function change(){
+    window.localStorage.setItem("tempsCronoTravail",tempsTravail.value )
+    window.localStorage.setItem("tempsCronoPause",tempsPause.value )
+    departMinutes = tempsTravail.value
+    departPause = tempsPause.value
+    
+    temps = departMinutes * 60
+    horloge();
+}
